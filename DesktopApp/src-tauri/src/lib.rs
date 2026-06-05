f002d73fb491659816574ba0024fbc94cea59a17
Paweł Szweda <szweda@cyberstudio.pl>
2026-06-05 13:36:46 +0200

    feat(desktop): add notification plugin and capabilities for session attention

--- DesktopApp/src-tauri/src/lib.rs
+++ DesktopApp/src-tauri/src/lib.rs
@@ -28,6 +28,7 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_notification::init())
         .manage(app_state)
         .setup(|app| {
             crate::remote::startup::init_remote_bridge(app.handle().clone());
