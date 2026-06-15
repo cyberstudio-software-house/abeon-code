befa6304aaa9dffffead66a73bf25592be26017d
Paweł Szweda <szweda@cyberstudio.pl>
2026-06-15 09:05:36 +0200

    feat(desktop): register updater and process plugins

--- DesktopApp/src-tauri/src/lib.rs
+++ DesktopApp/src-tauri/src/lib.rs
@@ -31,6 +31,8 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .manage(app_state)
         .setup(|app| {
             crate::remote::startup::init_remote_bridge(app.handle().clone());
